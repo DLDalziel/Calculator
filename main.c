abf5ffe4041403b0a690546f8efbf2cb0cefab9f
DLDalziel <deacondalziel@gmail.com>
2025-08-21 00:26:46 +0100

    Made ask function prompt and choices

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ Version: 1.0
 #include <stdio.h>
 
 int a, b; //Declare integers
+int choice;
 
 int Add(int a, int b) { //Add function
     return a + b;
@@ -27,14 +28,26 @@ int Divide(int a, int b) { //Divide function
     return a / b;
 }
 
-void AskUserNumber() { //Ask user for numbers
+void AskUserNumber() {
+    //Ask user for numbers
     printf("Please enter two number: ");
     scanf("%d%d", &a, &b);
 }
 
+    void AskUserFunction() {
+    //Ask user for function
+    printf("What would you like to do: \n");
+    printf("1.Add\n2.Subtract\n3.Multiply\n4.Divide\n"); //Choices
+    scanf("%d", &choice);
+}
+
 int main() {
 
-    AskUserNumber();
+    AskUserNumber(); //Call function
 
-    printf("Your numbers are %d and %d", a, b);
+    printf("Your numbers are %d and %d\n", a, b);
+
+    AskUserFunction(); //Call function
+
+    printf("Choice: %d", choice);
 }
