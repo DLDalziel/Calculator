5e16e5a1289369744df718dbed8b0951d3186c9a
DLDalziel <deacondalziel@gmail.com>
2025-08-21 00:18:23 +0100

    Made arithmetic

--- main.c
+++ main.c
@@ -14,6 +14,19 @@ Version: 1.0
 
 int a, b; //Declare integers
 
+int Add(int a, int b) { //Add function
+    return a + b;
+}
+int Subtract(int a, int b) { //Subtract function
+    return a - b;
+}
+int Multiply(int a, int b) { //Multiply function
+    return a * b;
+}
+int Divide(int a, int b) { //Divide function
+    return a / b;
+}
+
 void AskUserNumber() { //Ask user for numbers
     printf("Please enter two number: ");
     scanf("%d%d", &a, &b);
