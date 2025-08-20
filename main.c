889391935911999bd679fd43b59f9d5d8835cb9d
DLDalziel <deacondalziel@gmail.com>
2025-08-20 23:47:59 +0100

    Start program

---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+File name: Calculator
+Description: This program calculates basic arithmatic in C++
+
+Input: User inputted integers and a function
+Output: Answer
+Author: Deacon Dalziel
+
+Date: 20/08/2025
+Version: 1.0
+*/
