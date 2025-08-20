3fdb0702757f1efb6ed2ca9fd44328de8d616e4b
DLDalziel <deacondalziel@gmail.com>
2025-08-21 00:13:54 +0100

    Checking if receiving input

--- main.c
+++ main.c
@@ -9,3 +9,19 @@ Author: Deacon Dalziel
 Date: 20/08/2025
 Version: 1.0
 */
+
+#include <stdio.h>
+
+int a, b; //Declare integers
+
+void AskUserNumber() { //Ask user for numbers
+    printf("Please enter two number: ");
+    scanf("%d%d", &a, &b);
+}
+
+int main() {
+
+    AskUserNumber();
+
+    printf("Your numbers are %d and %d", a, b);
+}
